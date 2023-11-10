2b6929adb88e7b52227de8a1368a5bf683b8cc46
Julio Cesar <contato.jcsalerno@gmail.com>
2023-11-10 08:33:12 -0300

    Adição da Funcionalidade de adicionar

--- js/script.js
+++ js/script.js
@@ -7,12 +7,42 @@ const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 // Funções
+const saveTodo = (text) => {
+  const todo = document.createElement("div");
+  todo.classList.add("todo");
+
+  const todoTitle = document.createElement("h3");
+  todoTitle.innerText = text;
+  todo.appendChild(todoTitle);
+
+  const doneBtn = document.createElement("button");
+  doneBtn.classList.add("finish-todo");
+  doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+  todo.appendChild(doneBtn);
+
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("editBtn-todo");
+  editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+  todo.appendChild(editBtn);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("remove-todo");
+  deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  todo.appendChild(deleteBtn);
+
+  todoList.appendChild(todo);
+
+  todoInput.value = "";
+  todoInput.focus();
+};
 
 // Eventos
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
   const inputValue = todoInput.value;
+
   if (inputValue) {
-    console.log(inputValue);
+    saveTodo(inputValue);
   }
 });
