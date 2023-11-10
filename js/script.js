f0052f232283d1c4155962d6563310e2534077c4
Julio Cesar <contato.jcsalerno@gmail.com>
2023-11-10 08:18:01 -0300

    Começo do JavaScript

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Seleção de elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+// Funções
+
+// Eventos
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputValue = todoInput.value;
+  if (inputValue) {
+    console.log(inputValue);
+  }
+});
